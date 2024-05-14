9a22fb8ab2e808726117d89a3ac7fedcba5a9ad2
M Reza Khalafi <64053794+mrezakhalafi@users.noreply.github.com>
2024-05-15 00:14:18 +0700

    Tutorial Part 9 On 15/05/2024

--- app/Models/_Post.php
+++ app/Models/_Post.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 class Post extends Model
 {
-    use HasFactory;
+
+    // protected $table = "post_new";
 
     protected $fillable = ["title", "author", "slug", "body"];
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,7 @@
   <article class="py-8 mx-w-screen-md border-b border-gray-300">
     <a href="/posts/{{ $post['slug'] }}"><h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:underline">{{ $post['title'] }}</h2></a>
     <div class="text-base text-gray-500">
-      <a>{{ $post['author'] }} | 1 Januari 2024</a>
+      <a>{{ $post['author'] }} | {{ $post->created_at->diffForHumans() }}</a>
     </div>
     <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
     <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a> 
