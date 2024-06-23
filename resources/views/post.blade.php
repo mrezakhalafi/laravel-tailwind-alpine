112b06474bb606cc252a49dbd5fe2dec8de1ce19
M Reza Khalafi <64053794+mrezakhalafi@users.noreply.github.com>
2024-06-23 12:36:59 +0700

    Tutorial Part 15 On 23/06/2024

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,28 @@
 <x-layout>
 
-    <x-slot:title>{{ $title }}</x-slot:title>
-  
-    <article class="py-8 mx-w-screen-md">
-      <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:underline">{{ $post['title'] }}</h2>
-      <div class="text-base text-gray-500">
-        <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }} </a>| {{ $post->created_at->diffForHumans() }}</a>
-      </div>
-      <p class="my-4 font-light">{{ $post['body'] }}</p>
-      <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back To Posts</a> 
-  </article>
-  
-  </x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+
+  <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
+    <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
+        <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+          <header class="mb-4 lg:mb-6 not-format">
+              <a href="/posts" class="font-medium text-sm text-blue-600 hover:underline">&laquo; Back To Posts</a> 
+                <address class="flex items-center my-6 not-italic">
+                    <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                        <img class="mr-4 w-16 h-16 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Jese Leos">
+                        <div>
+                            <a href="/authors/{{ $post->author->id }}" rel="author" class="text-xl font-bold text-gray-900 dark:text-white hover:underline">{{ $post->author->name }}</a>
+                            <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate datetime="{{ $post->created_at->diffForHumans() }}" title="{{  $post->created_at->diffForHumans()  }}">{{ $post->created_at->diffForHumans() }}</time></p>
+                            <span class="bg-{{ $post->category->color }}-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                              <a href="/category/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+                            </span>
+                          </div>
+                    </div>
+                </address>
+                <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">{{ $post['title'] }}</h1>
+            </header>
+            <p>{{ $post['body'] }}</p>
+        </article>
+    </div>
+  </main>
+</x-layout>
